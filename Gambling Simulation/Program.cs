10aabdecfc303ced0b6ff474ae38b70eb6e1de24
poonambudde <buddepoonam123@gmail.com>
2022-03-11 08:32:17 +0530

    [Poonam] ADDED: welcome to gambler program

---
+++ Gambling Simulation/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Gambling_Simulation
+{
+    class Program
+    {
+        static void Main(string[] arg)
+        {
+            Console.WriteLine("Welcome to Gambler Program");
+            
+            int startingTotalAmount = 100;
+            int betsPerGame = 1;
+        }
+    }
+}
