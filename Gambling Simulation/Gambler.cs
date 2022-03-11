65671d344e2b83f86062121467a8c03542ca1556
poonambudde <97157799+poonambudde@users.noreply.github.com>
2022-03-11 17:47:27 +0530

    [Poonam] ADDED: Game starts

---
+++ Gambling Simulation/Gambler.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gambling_Simulation
+{
+   public class Gambler
+   {  
+       const int STAKE = 100;
+       const int BET = 1;
+   }    
